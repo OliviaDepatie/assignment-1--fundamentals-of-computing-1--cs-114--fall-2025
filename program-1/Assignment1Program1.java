73d0f8f8538269738e2dc6be4f94351fcd30d2c3
OliviaDepatie <depatie@hartford.edu>
2025-09-18 08:43:12 -0400

    Update Assignment1Program1.java

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -4,13 +4,13 @@ import java.util.Random;
 public class Assignment1Program1 {
   public static void main(String[] args) {
 
-    System.out.println("  OOOO    DDDD");
-    System.out.println(" O    O   D   D");
-    System.out.println(" O    O   D    D");
-    System.out.println(" O    O   D   D");
-    System.out.println("  OOOO    DDDD");
+    System.out.println(" OOOO    DDDD");
+    System.out.println("O    O   D   D");
+    System.out.println("O    O   D    D");
+    System.out.println("O    O   D   D");
+    System.out.println(" OOOO    DDDD");
 
-    System.out.println("\n Please enter a 5-character string:");
+    System.out.println("\nPlease enter a 5-character string:");
 
     String message;
     Scanner scan = new Scanner(System.in);
@@ -22,20 +22,20 @@ public class Assignment1Program1 {
     stringBuilder.reverse();
     String reversedMessage = stringBuilder.toString();
 
-    System.out.println("\n Please enter a number in Fahrenheit:");
+    System.out.println("\nPlease enter a number in Fahrenheit:");
 
     double fahrenheit = scan.nextDouble();
     double celsius = (fahrenheit - 32) * 5/9;
     celsius = Math.round(celsius * 10.0) / 10.0;
 
-    System.out.println("\n Random number generated. Continuing...");
+    System.out.println("\nRandom number generated. Continuing...");
 
     Random random = new Random();
     int min = 32;
     int max = 16384;
     int randomNumber = random.nextInt(max - min + 1) + min;
 
-    System.out.println("\n Your new string is: "
+    System.out.println("\nYour new string is: "
     + celsius + "" + reversedMessage + "" + randomNumber);
   }
 }
