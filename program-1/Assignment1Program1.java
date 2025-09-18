acb1f64f9110e39b2ed97e1a050be0d7143cd7f7
OliviaDepatie <depatie@hartford.edu>
2025-09-18 07:51:28 -0400

    Update Assignment1Program1.java

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -3,8 +3,6 @@ import java.util.Random;
 
 public class Assignment1Program1 {
   public static void main(String[] args) {
-    String message;
-    Scanner scan = new Scanner(System.in);
 
     System.out.println("  OOOO    DDDD");
     System.out.println(" O    O   D   D");
@@ -14,22 +12,20 @@ public class Assignment1Program1 {
 
     System.out.println("\n Please enter a 5-character string:");
 
+    String message;
+    Scanner scan = new Scanner(System.in);
+
     message = scan.nextLine();
 
     String trimmedMessage = message.substring(1,message.length()-1);
-
     StringBuilder stringBuilder = new StringBuilder(trimmedMessage);
-
     stringBuilder.reverse();
-
     String reversedMessage = stringBuilder.toString();
 
     System.out.println("\n Please enter a number in Fahrenheit:");
 
     double fahrenheit = scan.nextDouble();
-
     double celsius = (fahrenheit - 32) * 5/9;
-
     celsius = Math.round(celsius * 10.0) / 10.0;
 
     System.out.println("\n Random number generated. Continuing...");
@@ -41,6 +37,5 @@ public class Assignment1Program1 {
 
     System.out.println("\n Your new string is: "
     + celsius + "" + reversedMessage + "" + randomNumber);
-
   }
 }
